ffe8b26a7e16e32be185ffc7fc59afff451c3499
universeye <terrykuo501@gmail.com>
2025-04-22 17:23:35 +0800

    Initial setup

---
+++ NitraTakeHomeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NitraTakeHomeProject
+//
+//  Created by Terry Kuo on 2025/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NitraTakeHomeProject/NitraTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NitraTakeHomeProjectApp.swift
+//  NitraTakeHomeProject
+//
+//  Created by Terry Kuo on 2025/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NitraTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
